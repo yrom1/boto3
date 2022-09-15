aa946c48eb53df448e97c6cee8f1c537fe8b137a
yrom1 <ryanm.inbox@gmail.com>
2022-09-15 11:29:26 -0400

    Magic map

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 import boto3
 
-TABLE = "plotsV2"
+
+class Magic:
+    def __init__(self, table: str) -> None:
+        self.table = table
+
+    def put(self, key: str, value) -> None:
+        dynamodb = boto3.resource("dynamodb")
+        table = dynamodb.Table(self.table)
+        # NOTE if you want to support floats add this probably, untested
+        # x = json.loads(json.dumps(cartBefore), parse_float=Decimal)
+        response = table.put_item(
+            Item={
+                "key": key,
+                "value": value,
+            }
+        )
+        assert response["ResponseMetadata"]["HTTPStatusCode"] == 200
+
+    def get(self, key: str):
+        dynamodb = boto3.resource("dynamodb")
+        table = dynamodb.Table(self.table)
+        response = table.get_item(
+            Key={
+                "key": key,
+            }
+        )
+        return response["Item"]["value"]
 
 
-def put_item(key: str, value) -> None:
-    dynamodb = boto3.resource("dynamodb")
-    table = dynamodb.Table(TABLE)
-    # NOTE if you want to support floats add this probably, untested
-    # x = json.loads(json.dumps(cartBefore), parse_float=Decimal)
-    response = table.put_item(
-        Item={
-            "key": key,
-            "value": value,
-        }
-    )
-    assert response["ResponseMetadata"]["HTTPStatusCode"] == 200
-
-
-def get_item(key: str):
-    dynamodb = boto3.resource("dynamodb")
-    table = dynamodb.Table(TABLE)
-    response = table.get_item(
-        Key={
-            "key": key,
-        }
-    )
-    return response["Item"]["value"]
-
-
-put_item("test5", 42)
-x = get_item("test5")
-print(x)
+mp = Magic("plotsV2")
+mp.put("answer", 42)
+print(mp.get("answer"))
